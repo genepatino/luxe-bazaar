d7187dd2a1cba824c8af925ab606c7e40699de2e
Genesis Patiño <62777013+genepatino@users.noreply.github.com>
2024-02-27 09:45:14 -0500

    functional application

--- src/components/ShoppingCartPorducts/index.tsx
+++ src/components/ShoppingCartPorducts/index.tsx
@@ -15,7 +15,7 @@ import { Button } from "../ReturnProducts/styled";
 import { totalProductsPrice } from "../../utils";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
-import { IShoppingCart } from "../../redux/types";
+import { IShoppingCartProduct } from "../../redux/types";
 
 function ShoppingCartPorducts() {
   const [t] = useTranslation("global");
@@ -30,7 +30,7 @@ function ShoppingCartPorducts() {
 
   const dispatch = useAppDispatch();
 
-  const decrease = (product: IShoppingCart) => {
+  const decrease = (product: IShoppingCartProduct) => {
     const newProduct = {
       ...product,
       quantity: product.quantity > 1 ? product.quantity - 1 : product.quantity,
@@ -38,12 +38,12 @@ function ShoppingCartPorducts() {
     dispatch(updateShoppingCartProduct(newProduct));
   };
 
-  const increase = (product: IShoppingCart) => {
+  const increase = (product: IShoppingCartProduct) => {
     const newProduct = { ...product, quantity: product.quantity + 1 };
     dispatch(updateShoppingCartProduct(newProduct));
   };
 
-  const deleteProductFromCart = (product: IShoppingCart) => {
+  const deleteProductFromCart = (product: IShoppingCartProduct) => {
     dispatch(deleteProduct(product));
   };
 
@@ -52,44 +52,51 @@ function ShoppingCartPorducts() {
   };
 
   return (
-    <ShoppingContainer $display={shoppingCartPanel}>
-      <IoCloseCircleSharp className="icon-close" onClick={closeShoppingCart} />
-      <div className="list-products-details">
-        {shoppingCartProducts.map((product: IShoppingCart) => (
-          <ShoppingCartDetailsContainer key={product.id}>
-            <AiFillDelete
-              className="iconDeleteProduct"
-              onClick={() => deleteProductFromCart(product)}
-            />
+    <>
+      {shoppingCartProducts.length > 0 && (
+        <ShoppingContainer $display={shoppingCartPanel}>
+          <IoCloseCircleSharp
+            className="icon-close"
+            onClick={closeShoppingCart}
+          />
+          <div className="list-products-details">
+            {shoppingCartProducts.map((product: IShoppingCartProduct) => (
+              <ShoppingCartDetailsContainer key={product.id}>
+                <AiFillDelete
+                  className="iconDeleteProduct"
+                  onClick={() => deleteProductFromCart(product)}
+                />
+                <div>
+                  <img src={product.images[0]} alt={product.title} />
+                  <span>{product.title}</span>
+                </div>
+                <div className="quantity">
+                  <p className="button" onClick={() => decrease(product)}>
+                    -
+                  </p>
+                  <p>{product.quantity}</p>
+                  <p className="button" onClick={() => increase(product)}>
+                    +
+                  </p>
+                </div>
+                <span>{product.price * product.quantity}$</span>
+              </ShoppingCartDetailsContainer>
+            ))}
+          </div>
+          <TotalButton>
+            <Link to={"/order-summary"}>
+              <Button onClick={() => dispatch(showShoppingCartPanel(false))}>
+                {t("shoppingCart.buy-up")}
+              </Button>
+            </Link>
             <div>
-              <img src={product.images[0]} alt={product.title} />
-              <span>{product.title}</span>
+              <span>Total:</span>
+              <span>{totalProductsPrice(shoppingCartProducts)}$</span>
             </div>
-            <div className="quantity">
-              <p className="button" onClick={() => decrease(product)}>
-                -
-              </p>
-              <p>{product.quantity}</p>
-              <p className="button" onClick={() => increase(product)}>
-                +
-              </p>
-            </div>
-            <span>{product.price * product.quantity}$</span>
-          </ShoppingCartDetailsContainer>
-        ))}
-      </div>
-      <TotalButton>
-        <Link to={"/order-summary"}>
-          <Button onClick={() => dispatch(showShoppingCartPanel(false))}>
-            {t("shoppingCart.buy-up")}
-          </Button>
-        </Link>
-        <div>
-          <span>Total:</span>
-          <span>{totalProductsPrice(shoppingCartProducts)}$</span>
-        </div>
-      </TotalButton>
-    </ShoppingContainer>
+          </TotalButton>
+        </ShoppingContainer>
+      )}
+    </>
   );
 }
 
